5b2b780998a66bf378f81cf9981afddc3cce9c4d
Alex Cipollone <alex.cipollone10@gmail.com>
2023-10-19 13:48:48 -0400

    completed "Loading Spinner"

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 // Import useEffect to call this when the page loads
 import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
 
 function UserResults() {
   const [users, setUsers] = useState([]);
@@ -31,7 +32,7 @@ function UserResults() {
       </div>
     );
   } else {
-    return <h3>Loading...</h3>;
+    return <Spinner />;
   }
 }
 
